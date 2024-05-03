0b22ba5ffe688376a44cc9438a9b90552de48d20
rexlypenafloridaii <rpenafloridaii@gmail.com>
2024-05-02 21:00:11 -0500

    Add frontend authentication

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,9 @@
+import { LogoutLink } from "./LogoutLink";
 export function Header() {
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+        <a href="#">Home</a> | <a href="#">Link</a> | <LogoutLink />
       </nav>
     </header>
   );
