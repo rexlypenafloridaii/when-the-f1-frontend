b5989b77b1195672525c5373d4f8506e73a4042b
rexlypenafloridaii <rpenafloridaii@gmail.com>
2024-05-06 21:26:56 -0500

    Style login page and user access page

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,23 +3,35 @@ import { LogoutLink } from "./LogoutLink";
 export function Header() {
   return (
     <header>
-      <nav>
+      <nav className="bg-black text-rose-600 p-4">
         {localStorage.getItem("jwt") ? (
-          <li className="nav-item">
-            <LogoutLink />
-          </li>
+          <>
+            <a className="nav-item bg-white px-3 py-2 hover:bg-black">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </a>
+            <a className="nav-item  bg-white px-3 py-2 hover:bg-black">
+              <LogoutLink />
+            </a>
+          </>
         ) : (
           <>
-            <li className="nav-item">
+            <a className="nav-item bg-white px-3 py-2 hover:bg-black">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </a>
+            <a className="nav-item bg-white px-3 py-2 hover:bg-black">
               <Link className="nav-link" to="/signup">
                 Signup
               </Link>
-            </li>
-            <li className="nav-item">
+            </a>
+            <a className="nav-item  bg-white px-3 py-2 hover:bg-black">
               <Link className="nav-link" to="/login">
                 Login
               </Link>
-            </li>
+            </a>
           </>
         )}
       </nav>
